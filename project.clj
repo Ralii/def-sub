25868d996ffd9cdbf12152dd189fc6000146bb6f
lari.saukkone <lari.saukkonean1@gmail.com>
2024-09-22 08:58:13 +0300

    add test for def-sub

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject org.clojars.ralii/def-sub "0.1.2"
+(defproject org.clojars.ralii/def-sub "0.1.4"
   :description "Def sub"
   :url "http://example.com/Ralii/def-sub"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.11.60"]]
+                 [org.clojure/clojurescript "1.11.60"]
+                 [re-frame "1.4.3"]
+                 [day8.re-frame/test "0.1.5"]]
   :main ^:skip-aot def-sub.core
   :target-path "target/%s"
   :repositories [["releases" {:url "https://repo.clojars.org"
--- test/def_sub/core_test.clj
+++ test/def_sub/core_test.clj
@@ -1,7 +1,29 @@
 (ns def-sub.core-test
   (:require [clojure.test :refer :all]
-            [def-sub.core :refer :all]))
+            [def-sub.core :refer [def-sub]]
+            [day8.re-frame.test :as rf-test]
+            [re-frame.core :refer [reg-sub subscribe]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest integration
+  (testing "Whole pipeline should work")
+
+  (rf-test/run-test-sync
+
+   (reg-sub
+    :testing
+    (fn [_ _]
+      1))
+
+   (def-sub
+     testing-def-sub
+     (fn [_db [_ num]]
+       num))
+
+   (def-sub
+     testing-def-sub-2
+     (fn [_db [_]]
+       3))
+
+   (is (= 1 @(subscribe [:testing])))
+   (is (= 2 @(testing-def-sub 2)))
+   (is (= 3 @(testing-def-sub-2)))))
